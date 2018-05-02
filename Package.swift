ad69972072ebc13a980ed11565d2f0f513f76877
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-05-01 21:31:57 -0600

    Updated SDGCornerstone.

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         .library(name: "SDGCommandLineTestUtilities", targets: ["SDGCommandLineTestUtilities"])
     ],
     dependencies: [
-        .package(url: "https://github.com/SDGGiesbrecht/SDGCornerstone", .upToNextMinor(from: Version(0, 9, 2))),
+        .package(url: "https://github.com/SDGGiesbrecht/SDGCornerstone", .upToNextMinor(from: Version(0, 9, 3))),
         .package(url: "https://github.com/SDGGiesbrecht/SDGSwift", .upToNextMinor(from: Version(0, 1, 0)))
     ],
     targets: [
