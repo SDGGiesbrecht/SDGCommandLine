fa16319d3555614c63d3a6ef41f648ed504fd182
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-09-25 20:07:55 +0300

    Version 0.4.0

--- Workspace.swift
+++ Workspace.swift
@@ -17,7 +17,7 @@ import WorkspaceConfiguration
 let configuration = WorkspaceConfiguration()
 configuration._applySDGDefaults()
 
-configuration.documentation.currentVersion = Version(0, 3, 3)
+configuration.documentation.currentVersion = Version(0, 4, 0)
 
 configuration.documentation.projectWebsite = URL(string: "https://sdggiesbrecht.github.io/SDGCommandLine/SDGCommandLine")!
 configuration.documentation.documentationURL = URL(string: "https://sdggiesbrecht.github.io/SDGCommandLine")!
