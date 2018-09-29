3b0fb16610c21c48339d7842d5100b885c0ea3b0
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-09-29 12:53:52 +0300

    Removed spaces from directory names.

--- Package.swift
+++ Package.swift
@@ -48,8 +48,8 @@ let package = Package(
         .library(name: "SDGCommandLineTestUtilities", targets: ["SDGCommandLineTestUtilities"])
     ],
     dependencies: [
-        .package(url: "https://github.com/SDGGiesbrecht/SDGCornerstone", .upToNextMinor(from: Version(0, 11, 0))),
-        .package(url: "https://github.com/SDGGiesbrecht/SDGSwift", .upToNextMinor(from: Version(0, 3, 0)))
+        .package(url: "https://github.com/SDGGiesbrecht/SDGCornerstone", .branch("no‐spaces")),//.upToNextMinor(from: Version(0, 11, 0))),
+        .package(url: "https://github.com/SDGGiesbrecht/SDGSwift", .branch("no‐spaces"))//.upToNextMinor(from: Version(0, 3, 0)))
     ],
     targets: [
 
