8062004f9901c610d28b210301661c3f467ecd84
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-04-08 15:23:48 -0600

    Updated test manifests.

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,21 +1,6 @@
-/*
- LinuxMain.swift
-
- This source file is part of the SDGCommandLine open source project.
- https://sdggiesbrecht.github.io/SDGCommandLine
-
- Copyright ©2018–2019 Jeremy David Giesbrecht and the SDGCommandLine project contributors.
-
- Soli Deo gloria.
-
- Licensed under the Apache Licence, Version 2.0.
- See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
- */
-
 import XCTest
 
 import SDGCommandLineTests
-import SDGXCTestUtilities
 
 var tests = [XCTestCaseEntry]()
 tests += SDGCommandLineTests.__allTests()
