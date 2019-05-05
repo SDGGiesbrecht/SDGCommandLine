e8ff54c36d3b66da8295dd1cb99f025678e49654
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-05-04 20:02:22 -0600

    Removed redundant test utilities.

--- Tests/SDGCommandLineTests/InternalTests.swift
+++ Tests/SDGCommandLineTests/InternalTests.swift
@@ -72,85 +72,83 @@ class InternalTests : TestCase {
         testCustomStringConvertibleConformance(of: Options(), localizations: InterfaceLocalization.self, uniqueTestName: "None", overwriteSpecificationInsteadOfFailing: false)
     }
 
-    func testVersionSelection() {
+    func testVersionSelection() throws {
         FileManager.default.delete(.cache)
         defer { FileManager.default.delete(.cache) }
 
         let currentPackage = ProcessInfo.packageURL
         defer { ProcessInfo.packageURL = currentPackage }
 
-        XCTAssertErrorFree {
-            var ignored = Command.Output()
-            let testToolName = "tool"
-            try FileManager.default.withTemporaryDirectory(appropriateFor: nil) { temporaryDirectory in
-                let location = temporaryDirectory.appendingPathComponent(testToolName)
+        var ignored = Command.Output()
+        let testToolName = "tool"
+        try FileManager.default.withTemporaryDirectory(appropriateFor: nil) { temporaryDirectory in
+            let location = temporaryDirectory.appendingPathComponent(testToolName)
 
-                let testPackage = try PackageRepository(initializingAt: location, named: StrictString(location.lastPathComponent), type: .executable)
-                try Shell.default.run(command: ["git", "init"], in: testPackage.location)
+            let testPackage = try PackageRepository(initializingAt: location, named: StrictString(location.lastPathComponent), type: .executable)
+            try Shell.default.run(command: ["git", "init"], in: testPackage.location)
 
-                try "print(CommandLine.arguments.dropFirst().joined(separator: \u{22} \u{22}))".save(to: testPackage.location.appendingPathComponent("Sources/" + testToolName + "/main.swift"))
-                try testPackage.commitChanges(description: "Version 1.0.0")
-                try testPackage.tag(version: Version(1, 0, 0))
+            try "print(CommandLine.arguments.dropFirst().joined(separator: \u{22} \u{22}))".save(to: testPackage.location.appendingPathComponent("Sources/" + testToolName + "/main.swift"))
+            try testPackage.commitChanges(description: "Version 1.0.0")
+            try testPackage.tag(version: Version(1, 0, 0))
 
-                ProcessInfo.packageURL = testPackage.location
+            ProcessInfo.packageURL = testPackage.location
 
-                func postprocess(_ output: inout String) {
-                    output.replaceMatches(for: temporaryDirectory.absoluteString, with: "[Temporary Directory]")
-                    output.replaceMatches(for: temporaryDirectory.path, with: "[Temporary Directory]")
+            func postprocess(_ output: inout String) {
+                output.replaceMatches(for: temporaryDirectory.absoluteString, with: "[Temporary Directory]")
+                output.replaceMatches(for: temporaryDirectory.path, with: "[Temporary Directory]")
 
-                    let cacheDirectory = FileManager.default.url(in: .cache, at: "File").deletingLastPathComponent()
-                    output.replaceMatches(for: cacheDirectory.path, with: "[Cache]")
+                let cacheDirectory = FileManager.default.url(in: .cache, at: "File").deletingLastPathComponent()
+                output.replaceMatches(for: cacheDirectory.path, with: "[Cache]")
 
-                    output.scalars.replaceMatches(for: CompositePattern([
-                        LiteralPattern("\n".scalars),
-                        RepetitionPattern(ConditionalPattern({ $0 ∉ CharacterSet.whitespaces }), consumption: .lazy),
-                        LiteralPattern("\trefs/heads/master".scalars)
-                        ]), with: "\n[Commit Hash]\trefs/heads/master".scalars)
-                    output.scalars.replaceMatches(for: CompositePattern([
-                        LiteralPattern("Development/".scalars),
-                        RepetitionPattern(ConditionalPattern({ $0 ∉ CharacterSet.whitespaces }), consumption: .lazy),
-                        LiteralPattern("/".scalars)
-                        ]), with: "Development/[Commit Hash]/".scalars)
-                    output.scalars.replaceMatches(for: CompositePattern([
-                        LiteralPattern(".build/".scalars),
-                        RepetitionPattern(ConditionalPattern({ $0 ≠ "/" }), consumption: .lazy),
-                        LiteralPattern("/release".scalars)
-                        ]), with: ".build/[Operating System]/release".scalars)
-                    output.scalars.replaceMatches(for: CompositePattern([
-                        LiteralPattern("Cloning into \u{27}".scalars),
-                        RepetitionPattern(ConditionalPattern({ $0 ≠ "\u{27}" }), consumption: .lazy),
-                        LiteralPattern("\u{27}".scalars)
-                        ]), with: "Cloning into \u{27}...\u{27}".scalars)
-                    output.scalars.replaceMatches(for: CompositePattern([
-                        LiteralPattern("tool ".scalars),
-                        RepetitionPattern(ConditionalPattern({ $0 ≠ "\n" }), consumption: .lazy),
-                        LiteralPattern("/tool \u{2D}\u{2D}branch".scalars)
-                        ]), with: "tool [...]/tool \u{2D}\u{2D}branch".scalars)
-                    output.scalars.replaceMatches(for: CompositePattern([
-                        LiteralPattern("tool ".scalars),
-                        RepetitionPattern(ConditionalPattern({ $0 ≠ "\n" }), consumption: .lazy),
-                        LiteralPattern("/tool \u{2D}\u{2D}depth".scalars)
-                        ]), with: "tool [...]/tool \u{2D}\u{2D}depth".scalars)
-                }
-
-                // When the cache is empty...
-                testCommand(Tool.createCommand(), with: ["some‐invalid‐argument", "•use‐version", "1.0.0", "another‐invalid‐argument"], localizations: APILocalization.self, uniqueTestName: "Use Version (Empty Cache)", postprocess: postprocess, overwriteSpecificationInsteadOfFailing: false)
-
-                // When the cache exists...
-                testCommand(Tool.createCommand(), with: ["some‐invalid‐argument", "•use‐version", "1.0.0", "another‐invalid‐argument"], localizations: APILocalization.self, uniqueTestName: "Use Version (Cached)", postprocess: postprocess, overwriteSpecificationInsteadOfFailing: false)
-
-                // When the cache is empty...
-                testCommand(Tool.createCommand(), with: ["some‐invalid‐argument", "•use‐version", "development", "another‐invalid‐argument"], localizations: APILocalization.self, uniqueTestName: "Use Development (Empty Cache)", postprocess: postprocess, overwriteSpecificationInsteadOfFailing: false)
-
-                // When the cache exists...
-                testCommand(Tool.createCommand(), with: ["some‐invalid‐argument", "•use‐version", "development", "another‐invalid‐argument"], localizations: APILocalization.self, uniqueTestName: "Use Development (Cached)", postprocess: postprocess, overwriteSpecificationInsteadOfFailing: false)
-
-                // Looking for version when it does not exist...
-                testCommand(Tool.createCommand(), with: ["some‐invalid‐argument", "another‐invalid‐argument"], localizations: APILocalization.self, uniqueTestName: "Without Version", postprocess: postprocess, overwriteSpecificationInsteadOfFailing: false)
-
-                // Asking for something which is not a version...
-                testCommand(Tool.createCommand(), with: ["some‐invalid‐argument", "•use‐version", "not‐a‐version", "another‐invalid‐argument"], localizations: APILocalization.self, uniqueTestName: "Use Invalid Version", postprocess: postprocess, overwriteSpecificationInsteadOfFailing: false)
+                output.scalars.replaceMatches(for: CompositePattern([
+                    LiteralPattern("\n".scalars),
+                    RepetitionPattern(ConditionalPattern({ $0 ∉ CharacterSet.whitespaces }), consumption: .lazy),
+                    LiteralPattern("\trefs/heads/master".scalars)
+                    ]), with: "\n[Commit Hash]\trefs/heads/master".scalars)
+                output.scalars.replaceMatches(for: CompositePattern([
+                    LiteralPattern("Development/".scalars),
+                    RepetitionPattern(ConditionalPattern({ $0 ∉ CharacterSet.whitespaces }), consumption: .lazy),
+                    LiteralPattern("/".scalars)
+                    ]), with: "Development/[Commit Hash]/".scalars)
+                output.scalars.replaceMatches(for: CompositePattern([
+                    LiteralPattern(".build/".scalars),
+                    RepetitionPattern(ConditionalPattern({ $0 ≠ "/" }), consumption: .lazy),
+                    LiteralPattern("/release".scalars)
+                    ]), with: ".build/[Operating System]/release".scalars)
+                output.scalars.replaceMatches(for: CompositePattern([
+                    LiteralPattern("Cloning into \u{27}".scalars),
+                    RepetitionPattern(ConditionalPattern({ $0 ≠ "\u{27}" }), consumption: .lazy),
+                    LiteralPattern("\u{27}".scalars)
+                    ]), with: "Cloning into \u{27}...\u{27}".scalars)
+                output.scalars.replaceMatches(for: CompositePattern([
+                    LiteralPattern("tool ".scalars),
+                    RepetitionPattern(ConditionalPattern({ $0 ≠ "\n" }), consumption: .lazy),
+                    LiteralPattern("/tool \u{2D}\u{2D}branch".scalars)
+                    ]), with: "tool [...]/tool \u{2D}\u{2D}branch".scalars)
+                output.scalars.replaceMatches(for: CompositePattern([
+                    LiteralPattern("tool ".scalars),
+                    RepetitionPattern(ConditionalPattern({ $0 ≠ "\n" }), consumption: .lazy),
+                    LiteralPattern("/tool \u{2D}\u{2D}depth".scalars)
+                    ]), with: "tool [...]/tool \u{2D}\u{2D}depth".scalars)
             }
+
+            // When the cache is empty...
+            testCommand(Tool.createCommand(), with: ["some‐invalid‐argument", "•use‐version", "1.0.0", "another‐invalid‐argument"], localizations: APILocalization.self, uniqueTestName: "Use Version (Empty Cache)", postprocess: postprocess, overwriteSpecificationInsteadOfFailing: false)
+
+            // When the cache exists...
+            testCommand(Tool.createCommand(), with: ["some‐invalid‐argument", "•use‐version", "1.0.0", "another‐invalid‐argument"], localizations: APILocalization.self, uniqueTestName: "Use Version (Cached)", postprocess: postprocess, overwriteSpecificationInsteadOfFailing: false)
+
+            // When the cache is empty...
+            testCommand(Tool.createCommand(), with: ["some‐invalid‐argument", "•use‐version", "development", "another‐invalid‐argument"], localizations: APILocalization.self, uniqueTestName: "Use Development (Empty Cache)", postprocess: postprocess, overwriteSpecificationInsteadOfFailing: false)
+
+            // When the cache exists...
+            testCommand(Tool.createCommand(), with: ["some‐invalid‐argument", "•use‐version", "development", "another‐invalid‐argument"], localizations: APILocalization.self, uniqueTestName: "Use Development (Cached)", postprocess: postprocess, overwriteSpecificationInsteadOfFailing: false)
+
+            // Looking for version when it does not exist...
+            testCommand(Tool.createCommand(), with: ["some‐invalid‐argument", "another‐invalid‐argument"], localizations: APILocalization.self, uniqueTestName: "Without Version", postprocess: postprocess, overwriteSpecificationInsteadOfFailing: false)
+
+            // Asking for something which is not a version...
+            testCommand(Tool.createCommand(), with: ["some‐invalid‐argument", "•use‐version", "not‐a‐version", "another‐invalid‐argument"], localizations: APILocalization.self, uniqueTestName: "Use Invalid Version", postprocess: postprocess, overwriteSpecificationInsteadOfFailing: false)
         }
     }
 
