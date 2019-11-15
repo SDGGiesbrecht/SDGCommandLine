f13cfa2bee68db394ef6abd831da9429b36179c9
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-11-14 21:38:23 -0600

    Updated Workspace.

--- Tests/SDGCommandLineTests/XCTestManifests.swift
+++ Tests/SDGCommandLineTests/XCTestManifests.swift
@@ -1,3 +1,17 @@
+/*
+ XCTestManifests.swift
+
+ This source file is part of the SDGCommandLine open source project.
+ https://sdggiesbrecht.github.io/SDGCommandLine
+
+ Copyright ©2019 Jeremy David Giesbrecht and the SDGCommandLine project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
 #if !canImport(ObjectiveC)
 import XCTest
 
