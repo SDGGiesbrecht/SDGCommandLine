5fcd30dfa2e6eae4f28309b64f205bbbe8b9505b
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-02-21 15:54:53 -0600

    Refreshed.

---
+++ .github/workflows/Windows/WindowsMain.swift
@@ -0,0 +1,83 @@
+/*
+ WindowsMain.swift
+
+ This source file is part of the SDGCommandLine open source project.
+ https://sdggiesbrecht.github.io/SDGCommandLine
+
+ Copyright ©2020 Jeremy David Giesbrecht and the SDGCommandLine project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+import XCTest
+
+@testable import SDGExportedCommandLineInterfaceTests
+@testable import SDGCommandLineTests
+
+extension APITests {
+  static let windowsTests: [XCTestCaseEntry] = [
+    testCase([
+      ("testCommandInterface", testCommandInterface),
+    ])
+  ]
+}
+
+extension APITests {
+  static let windowsTests: [XCTestCaseEntry] = [
+    testCase([
+      ("testArgumentType", testArgumentType),
+      ("testCommand", testCommand),
+      ("testCommandError", testCommandError),
+      ("testDirectArgument", testDirectArgument),
+      ("testEnumerationOption", testEnumerationOption),
+      ("testFormatting", testFormatting),
+      ("testHelp", testHelp),
+      ("testLanguage", testLanguage),
+      ("testLocalizations", testLocalizations),
+      ("testNoColour", testNoColour),
+      ("testOption", testOption),
+      ("testVersion", testVersion),
+    ])
+  ]
+}
+
+extension ReadMeExampleTests {
+  static let windowsTests: [XCTestCaseEntry] = [
+    testCase([
+      ("testParrot", testParrot),
+    ])
+  ]
+}
+
+extension InternalTests {
+  static let windowsTests: [XCTestCaseEntry] = [
+    testCase([
+      ("testDirectArguments", testDirectArguments),
+      ("testEmptyCache", testEmptyCache),
+      ("testExportInterface", testExportInterface),
+      ("testSetLanguage", testSetLanguage),
+      ("testOptions", testOptions),
+      ("testVersionSelection", testVersionSelection),
+      ("testVersionSubcommand", testVersionSubcommand),
+    ])
+  ]
+}
+
+extension TestCase {
+  static let windowsTests: [XCTestCaseEntry] = [
+    testCase([
+      ])
+  ]
+}
+
+var tests = [XCTestCaseEntry]()
+tests += APITests.windowsTests
+tests += APITests.windowsTests
+tests += ReadMeExampleTests.windowsTests
+tests += InternalTests.windowsTests
+tests += TestCase.windowsTests
+
+XCTMain(tests)
