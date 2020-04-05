8a322233f3de2996a7e1e7f0810296b7b4395cc6
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-04-05 14:49:14 -0600

    Filled in missing method.

---
+++ Sources/SDGCommandLine/Error.swift
@@ -0,0 +1,23 @@
+/*
+ Error.swift
+
+ This source file is part of the SDGCommandLine open source project.
+ https://sdggiesbrecht.github.io/SDGCommandLine
+
+ Copyright ©2020 Jeremy David Giesbrecht and the SDGCommandLine project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+#if os(WASI)
+  extension Error {
+
+    // #workaround(workspace version 0.32.0, Web is missing this property.)
+    internal var localizedDescription: String {
+      return String(describing: self)
+    }
+  }
+#endif
