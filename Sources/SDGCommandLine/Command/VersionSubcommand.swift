045bdb3bf411e0cbd6f713bcd3c2cf4e0678115a
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-10-07 20:34:02 -0600

    Updated web Foundation workaround.

--- Sources/SDGCommandLine/Command/VersionSubcommand.swift
+++ Sources/SDGCommandLine/Command/VersionSubcommand.swift
@@ -12,7 +12,7 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
-// #workaround(Swift 5.2.4, Web doesn’t have Foundation yet.)
+// #workaround(Swift 5.3, Web doesn’t have Foundation yet.)
 #if !os(WASI)
   import Foundation
 #endif
@@ -44,7 +44,7 @@ extension Command {
       }
     })
 
-  // #workaround(Swift 5.2.4, Web doesn’t have Foundation yet.)
+  // #workaround(Swift 5.3, Web doesn’t have Foundation yet.)
   #if !os(WASI)
     internal static let version = Command(
       name: versionName,
