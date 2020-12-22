4d12e91cd0e1594a1cb21f1be5c173e6e43a7b4c
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-12-21 18:57:48 -0600

    ...

--- Package.swift
+++ Package.swift
@@ -266,10 +266,10 @@ let package = Package(
 // #warning("Debugging.")
 if ProcessInfo.processInfo.environment["TARGETING_WEB"] == "true" {
   package.targets.removeAll(where: { Set([
-    "SDGCommandLine",
+    // "SDGCommandLine",
     // "SDGCommandLineTestUtilities",
     // "SDGExportedCommandLineInterface",
-    "SDGCommandLineLocalizations",
+    // "SDGCommandLineLocalizations",
     "SDGCommandLineTests",
     "SDGExportedCommandLineInterfaceTests",
     "TestTool",
