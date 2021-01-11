d8be18236a9cf8c673a91c113291c7df690749a8
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-01-10 21:01:18 -0600

    Added manifest workarounds.

--- Package.swift
+++ Package.swift
@@ -287,6 +287,32 @@ for target in package.targets {
   ])
 }
 
+if ProcessInfo.processInfo.environment["TARGETING_TVOS"] == "true" {
+  // #workaround(xcodebuild -version 12.3, Tool targets don’t work on tvOS.) @exempt(from: unicode)
+  package.targets.removeAll(where: { $0.name.hasSuffix("‐tool") })
+  for target in package.targets {
+    target.dependencies.removeAll(where: { "\($0)".contains("‐tool") })
+  }
+}
+
+if ProcessInfo.processInfo.environment["TARGETING_IOS"] == "true" {
+  // #workaround(xcodebuild -version 12.3, Tool targets don’t work on iOS.) @exempt(from: unicode)
+  package.targets.removeAll(where: { $0.name.hasSuffix("‐tool") })
+  for target in package.targets {
+    target.dependencies.removeAll(where: { "\($0)".contains("‐tool") })
+  }
+}
+
+if ProcessInfo.processInfo.environment["TARGETING_WATCHOS"] == "true" {
+  // #workaround(xcodebuild -version 12.3, Test targets don’t work on watchOS.) @exempt(from: unicode)
+  package.targets.removeAll(where: { $0.isTest })
+  // #workaround(xcodebuild -version 12.3, Tool targets don’t work on watchOS.) @exempt(from: unicode)
+  package.targets.removeAll(where: { $0.name.hasSuffix("‐tool") })
+  for target in package.targets {
+    target.dependencies.removeAll(where: { "\($0)".contains("‐tool") })
+  }
+}
+
 // Windows Tests (Generated automatically by Workspace.)
 import Foundation
 if ProcessInfo.processInfo.environment["TARGETING_WINDOWS"] == "true" {
