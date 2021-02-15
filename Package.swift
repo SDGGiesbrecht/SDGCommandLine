43c67373d356d549e81af20005ccf4c0c10bed79
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-02-14 21:12:31 -0600

    Updated workarounds.

--- Package.swift
+++ Package.swift
@@ -288,7 +288,7 @@ for target in package.targets {
 }
 
 if ProcessInfo.processInfo.environment["TARGETING_TVOS"] == "true" {
-  // #workaround(xcodebuild -version 12.3, Tool targets don’t work on tvOS.) @exempt(from: unicode)
+  // #workaround(xcodebuild -version 12.4, Tool targets don’t work on tvOS.) @exempt(from: unicode)
   package.targets.removeAll(where: { $0.name.hasSuffix("‐tool") })
   for target in package.targets {
     target.dependencies.removeAll(where: { "\($0)".contains("‐tool") })
@@ -296,7 +296,7 @@ if ProcessInfo.processInfo.environment["TARGETING_TVOS"] == "true" {
 }
 
 if ProcessInfo.processInfo.environment["TARGETING_IOS"] == "true" {
-  // #workaround(xcodebuild -version 12.3, Tool targets don’t work on iOS.) @exempt(from: unicode)
+  // #workaround(xcodebuild -version 12.4, Tool targets don’t work on iOS.) @exempt(from: unicode)
   package.targets.removeAll(where: { $0.name.hasSuffix("‐tool") })
   for target in package.targets {
     target.dependencies.removeAll(where: { "\($0)".contains("‐tool") })
@@ -304,9 +304,9 @@ if ProcessInfo.processInfo.environment["TARGETING_IOS"] == "true" {
 }
 
 if ProcessInfo.processInfo.environment["TARGETING_WATCHOS"] == "true" {
-  // #workaround(xcodebuild -version 12.3, Test targets don’t work on watchOS.) @exempt(from: unicode)
+  // #workaround(xcodebuild -version 12.4, Test targets don’t work on watchOS.) @exempt(from: unicode)
   package.targets.removeAll(where: { $0.isTest })
-  // #workaround(xcodebuild -version 12.3, Tool targets don’t work on watchOS.) @exempt(from: unicode)
+  // #workaround(xcodebuild -version 12.4, Tool targets don’t work on watchOS.) @exempt(from: unicode)
   package.targets.removeAll(where: { $0.name.hasSuffix("‐tool") })
   for target in package.targets {
     target.dependencies.removeAll(where: { "\($0)".contains("‐tool") })
