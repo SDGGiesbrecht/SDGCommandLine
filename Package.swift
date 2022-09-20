b35d88a2c34d30a6ff4cd1314239ea26bed6e4e0
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-09-19 21:12:33 -0600

    Updated dependencies.

--- Package.swift
+++ Package.swift
@@ -142,7 +142,7 @@ let package = Package(
     ),
     .package(
       url: "https://github.com/SDGGiesbrecht/SDGSwift",
-      from: Version(10, 0, 0)
+      from: Version(11, 0, 0)
     ),
   ],
   targets: [
--- Tests/SDGCommandLineTests/InternalTests.swift
+++ Tests/SDGCommandLineTests/InternalTests.swift
@@ -155,8 +155,24 @@ class InternalTests: TestCase {
             named: StrictString(location.lastPathComponent),
             type: .executable
           ).get()
+          // #workaround(While stradling versions.)
+          #if compiler(<5.7)
+            let manifestLocation = testPackage.location.appendingPathComponent("Package.swift")
+            var manifest = try String(from: manifestLocation)
+            manifest.replaceMatches(
+              for: "swift\u{2D}tools\u{2D}version: 5.7",
+              with: "swift\u{2D}tools\u{2D}version: 5.6"
+            )
+            try manifest.save(to: manifestLocation)
+          #endif
           _ = try Shell.default.run(command: ["git", "init"], in: testPackage.location).get()
 
+          try? FileManager.default.removeItem(
+            at: testPackage.location
+              .appendingPathComponent("Sources")
+              .appendingPathComponent(testToolName)
+              .appendingPathComponent("\(testToolName).swift")
+          )
           try "print(CommandLine.arguments.dropFirst().joined(separator: \u{22} \u{22}))".save(
             to: testPackage.location.appendingPathComponent(
               "Sources/" + testToolName + "/main.swift"
