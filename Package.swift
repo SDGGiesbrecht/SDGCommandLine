91a6ff6e560a7eb1c41e83ceaa5e942781de91de
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2023-05-18 22:43:03 -0600

    Updated Workspace.

--- Package.swift
+++ Package.swift
@@ -304,3 +304,8 @@ for target in package.targets {
     .define("PLATFORM_USES_SEPARATE_TEST_BUNDLE", .when(platforms: [.macOS])),
   ])
 }
+
+// #workaround(Swift 5.7.2, Hardware compatibility; tools version does not reflect support.))
+#if compiler(<5.8) && !os(macOS)
+  #error("Swift 5.7 is only supported on macOS, tvOS, iOS and watchOS; elsewhere, please use Swift 5.8 or select an older version of SDGCommandLine.")
+#endif
