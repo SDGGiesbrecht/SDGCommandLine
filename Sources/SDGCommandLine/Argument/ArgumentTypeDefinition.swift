b57182c8377ac8bd07373740858c33e2ff1b7e99
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2023-05-25 18:14:15 -0600

    ...

--- Sources/SDGCommandLine/Argument/ArgumentTypeDefinition.swift
+++ Sources/SDGCommandLine/Argument/ArgumentTypeDefinition.swift
@@ -34,21 +34,20 @@ where Type: Sendable {
     syntaxDescription: UserFacing<StrictString, D>,
     parse: @Sendable @escaping (_ argument: StrictString) -> Type?
   ) {
+    identifier = name.resolved(for: N.fallbackLocalization)
     // #warning("Debugging...")
-    identifier = ""
+    /*let sendableName: @Sendable () -> StrictString = { name.resolved() }
+    localizedName = sendableName
+    let sendableDescription: @Sendable () -> StrictString = { syntaxDescription.resolved() }
+    localizedDescription = sendableDescription
+    self.parse = parse*/
+
+    // #warning("Debugging...")
     let strictString: @Sendable () -> StrictString = { return "" }
     localizedName = strictString
     localizedDescription = strictString
     let type: @Sendable (StrictString) -> Type? = { _ in return nil }
     self.parse = type
-
-    // #warning("Debugging...")
-    /*identifier = name.resolved(for: N.fallbackLocalization)
-    let sendableName: @Sendable () -> StrictString = { name.resolved() }
-    localizedName = sendableName
-    let sendableDescription: @Sendable () -> StrictString = { syntaxDescription.resolved() }
-    localizedDescription = sendableDescription
-    self.parse = parse*/
   }
 
   // MARK: - Properties
